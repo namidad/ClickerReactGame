dbfaefee43b5ccffa59c11e06a76162fafaa3a52
Oliwier Salamon <salamonoliwier@gmail.com>
2018-09-19 13:22:54 +0200

    initial commit

---
+++ js/projekt.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    class App extends React.Component {
+        render() {
+            return (
+               null
+            );
+
+        }
+    }
+
+    ReactDOM.render(
+        <App/>,
+        document.getElementById('app')
+    );
+
+});
+
